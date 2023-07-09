684cf0de23ae552a173cf57115c8baaca7a7aa95
TrinhThuong2001 <88668726+JasmineVan@users.noreply.github.com>
2023-07-10 01:17:11 +0700

    Variable, Constant, Conditional statement, Switch-case and Interface

--- main.go
+++ main.go
@@ -2,27 +2,61 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"math"
+	"time"
 )
 
-// Structure
-type Car struct {
-	Name        string
-	Age         int
-	ModelNumber int
-}
-
-func arrayInit() {
-	myIntArray := []int{1, 2, 3, 4, 5}
-	myStringArray := []string{"Hi", "Im"}
-	myStringArray = append(myStringArray, "ThuongTV")
-	fmt.Println(myIntArray, myStringArray)
-}
-
 func main() {
-	fmt.Println(quote.Go())
-	arrayInit()
-	c := Car{"Ford", 3, 5}
-	fmt.Println(c)
+	// Hello World
+	fmt.Println("hello world")
+	// Value
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	//Variables
+	myName := "ThuongTV"
+	fmt.Println(myName)
+	// Constant
+	const s string = "ThuongTV Dep Trai"
+	const r = 3.14
+	fmt.Println(math.Pi - r)
+	// Loop: For
+	for i := 0; i < 10; i++ {
+		fmt.Println("Anh xin loi Phuong Anh")
+	}
+	// Conditional statement
+	for j := 0; j < 10; j++ {
+		if j%2 == 0 {
+			continue
+		} else {
+			fmt.Println(j)
+		}
+	}
+	// Switch - case
+	currentTime := time.Now()
+	switch currentTime.Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Its the weekend")
+	case time.Monday:
+		fmt.Println("Its the beginning of the week")
+	default:
+		fmt.Println("Work day")
+	}
+	// Interface
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
